2dd06003a592f1317cf67178215796575a8a5233
Yixing Lao <yixing.lao@gmail.com>
2021-01-23 01:38:11 -0800

    format

--- joinjanusvideoroom.js
+++ joinjanusvideoroom.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 /*
-* NodeJS example to send a webrtc-streamer stream to janus-gateway
-*/
+ * NodeJS example to send a webrtc-streamer stream to janus-gateway
+ */
 
 // decode arguments
 if (process.argv.length < 4) {
-    console.log("Usage: " + __filename + " <webrtc-streamer url> <videourl> <janus url> <janus room>");
-    process.exit(-1);
+  console.log(
+    "Usage: " +
+      __filename +
+      " <webrtc-streamer url> <videourl> <janus url> <janus room>"
+  );
+  process.exit(-1);
 }
 var webrtcstreamerurl = process.argv[2];
 console.log("webrtcstreamerurl: " + webrtcstreamerurl);
@@ -14,16 +18,14 @@ var videourl = process.argv[3];
 console.log("videourl: " + videourl);
 var janusRoomUrl = process.argv[4];
 console.log("janusRoomUrl: " + janusRoomUrl);
-var roomId = 1234
+var roomId = 1234;
 if (process.argv.length >= 5) {
-	roomId = process.argv[5];
+  roomId = process.argv[5];
 }
 console.log("roomId: " + roomId);
 
 global.fetch = require("node-fetch");
-var JanusVideoRoom = require("./html/janusvideoroom.js"); 
+var JanusVideoRoom = require("./html/janusvideoroom.js");
 var janus = new JanusVideoRoom(janusRoomUrl, webrtcstreamerurl);
 
-janus.join(roomId,videourl,"video");
-
-
+janus.join(roomId, videourl, "video");
--- joinxmpproom.js
+++ joinxmpproom.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 /*
-* NodeJS example to send a webrtc-streamer stream to jitsi
-*/
+ * NodeJS example to send a webrtc-streamer stream to jitsi
+ */
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 // decode arguments
 if (process.argv.length <= 4) {
-    console.log("Usage: " + __filename + " <webrtc-streamer url> <videourl> <xmpp url> <xmpp room>");
-    process.exit(-1);
+  console.log(
+    "Usage: " +
+      __filename +
+      " <webrtc-streamer url> <videourl> <xmpp url> <xmpp room>"
+  );
+  process.exit(-1);
 }
 var webrtcstreamerurl = process.argv[2];
 console.log("webrtcstreamerurl: " + webrtcstreamerurl);
@@ -14,9 +18,9 @@ var videourl = process.argv[3];
 console.log("videourl: " + videourl);
 var xmppRoomUrl = process.argv[4];
 console.log("xmppRoomUrl: " + xmppRoomUrl);
-var xmppRoomId = "testroom"
+var xmppRoomId = "testroom";
 if (process.argv.length >= 5) {
-	xmppRoomId = process.argv[5];
+  xmppRoomId = process.argv[5];
 }
 console.log("xmppRoomId: " + xmppRoomId);
 
@@ -25,7 +29,9 @@ const { JSDOM } = jsdom;
 const { window } = new JSDOM("");
 
 global.jquery = require("jquery")(window);
-global.$ = (selector,context) => {return new jquery.fn.init(selector,context); };
+global.$ = (selector, context) => {
+  return new jquery.fn.init(selector, context);
+};
 
 global.window = window;
 global.document = window.document;
@@ -38,17 +44,15 @@ global.$iq = strophe.$iq;
 //global.Strophe.log = console.log;
 
 require("strophejs-plugin-disco");
-require("strophejs-plugin-muc"); 
+require("strophejs-plugin-muc");
 
 global.SDP = require("strophe.jingle/strophe.jingle.sdp.js");
 
 global.fetch = require("node-fetch");
-var XMPPVideoRoom = require("./html/xmppvideoroom.js"); 
+var XMPPVideoRoom = require("./html/xmppvideoroom.js");
 
-			
 var xmpp = new XMPPVideoRoom(xmppRoomUrl, webrtcstreamerurl);
-var username = "user"+Math.random().toString(36).slice(2);
+var username = "user" + Math.random().toString(36).slice(2);
 console.log("join " + xmppRoomId + "/" + username);
 
-xmpp.join(xmppRoomId,videourl,username);
-
+xmpp.join(xmppRoomId, videourl, username);

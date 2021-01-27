18c2862be8b2e0f3988893a70a5b9aae61c40da5
Yixing Lao <yixing.lao@gmail.com>
2021-01-26 22:36:38 -0800

    first build step

--- webrtc.cmake
+++ webrtc.cmake
@@ -40,17 +40,39 @@ endif()
 file(WRITE ${CMAKE_BINARY_DIR}/args.gn ${WEBRTCARGS})
 # message(FATAL_ERROR "WEBRTCARGS: ${WEBRTCARGS}")
 
+set(NINJA_TARGET
+    webrtc
+    rtc_json
+    jsoncpp
+    builtin_video_decoder_factory
+    builtin_video_encoder_factory
+    peerconnection
+    p2p_server_utils
+    task_queue
+    default_task_queue_factory
+)
+
 ExternalProject_Add(
     ext_webrtc
     PREFIX webrtc
     DOWNLOAD_COMMAND rm -rf ext_webrtc
     COMMAND cp -ar /home/yixing/repo/webrtc ext_webrtc
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/args.gn <SOURCE_DIR>/src/out/${WEBRTCBUILD}/args.gn
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/args.gn
+        <SOURCE_DIR>/src/out/${WEBRTCBUILD}/args.gn
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
     BUILD_ALWAYS ON
     BUILD_IN_SOURCE ON
+)
+
+ExternalProject_Add_Step(ext_webrtc build_obj
+    COMMAND export PATH=$PATH:/home/yixing/repo/depot_tools
+    COMMAND /home/yixing/repo/depot_tools/gn gen .
+    COMMAND /home/yixing/repo/depot_tools/ninja ${NINJA_TARGET}
+    WORKING_DIRECTORY <SOURCE_DIR>/src/out/${WEBRTCBUILD}
+    DEPENDEES build
+    DEPENDERS install
 )
 
 set(WEBRTC_INCLUDE_DIR "") # "/" is critical.
